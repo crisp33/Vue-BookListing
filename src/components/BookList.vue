26bcd4976636f9471d882b19c23ee9f5c3e1bd86
Steve C <steve.crisp@me.com>
2020-09-24 20:25:41 -0700

    Completed Module 5

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -6,6 +6,8 @@
                
                 </book-item>
         </ul>
+        <hr>
+        <book-form @addBook='appendBook'></book-form>
 
     </div>
 </template>
@@ -13,11 +15,13 @@
 
 <script>
 import BookItem from './BookItem';
+import BookForm from './BookForm';
 
 export default {
     name: 'BookList',
     components: {
-        BookItem
+        BookItem,
+        BookForm
     },
     data () {
         return {
@@ -27,6 +31,11 @@ export default {
                 {title: 'American Gods', author: 'Neil Gaiman'},
                 {title: 'Amusing Ourselves to Death', author: 'Neil Postman'},
             ]
+        }
+    },
+    methods: {
+        appendBook(bookTitle, bookAuthor){
+            this.books.push({title: bookTitle, author: bookAuthor})
         }
     }
 }
