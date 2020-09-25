4126310f8b5aa0000bef67abe4f7c6172f589850
Steve C <steve.crisp@me.com>
2020-09-24 21:01:03 -0700

    Completed Module 5

--- src/components/BookForm.vue
+++ src/components/BookForm.vue
@@ -1,5 +1,5 @@
 <template>
-    <form v-on: submit.prevent="bookSubmit(bookTitle, bookAuthor)" class="" action="#" method="post">
+    <form v-on: submit.revent="bookSubmit(bookTitle, bookAuthor)" class="" action="#" method="post">
         <input v-model="bookTitle" type="text" name="title" value="" placeholder="Book Title">
         <input v-model="bookAuthor" type="text" name="author" value="" placeholder="Book Author">
         <button type="submit" name="button">Add Book</button>
@@ -18,7 +18,7 @@ export default {
     },
     methods: {
         bookSubmit(bookTitle, bookAuthor){
-            this.$emit('addBook', booktitle, bookAuthor)
+            this.$emit('addBook', bookTitle, bookAuthor)
         }
     }
 }
